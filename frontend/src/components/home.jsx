4b54f7eb28280871e7bcefa80f5a22b95a4710c7
lucas-science <lucaslhomme01@gmail.com>
2022-03-06 21:49:03 +0100

    try to add react router

---
+++ frontend/src/components/home.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+    // différente route renvoyant un composant react
+    render() {
+      return (
+        <div>
+            <p>TEST</p>
+        </div>
+      );
+    }
+  }
+  
+  export default Home;
+  
